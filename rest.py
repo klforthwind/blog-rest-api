ea62c30c0dececf2856ee0e235b875bc8218dc45
klforthwind <kl.forthwind@gmail.com>
2019-05-18 16:28:14 -0500

    add the four standard http request methods

--- rest.py
+++ rest.py
@@ -4,4 +4,15 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+class Blog(Resource):
+
+    def get(self, name):
+
+    def post(self, name):
+
+    def put(self, name):
+
+    def delete(self, name):
+
+
 app.run(debug=True)
