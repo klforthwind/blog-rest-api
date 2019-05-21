e5c08dd18d02a18408e9785cf9e23b630a57af70
klforthwind <kl.forthwind@gmail.com>
2019-05-21 07:57:10 +0000

    simplify psql.py

--- PSQL.py
+++ PSQL.py
@@ -1,21 +1,16 @@
 from login import *
 import psycopg2
 
-
 hostname = 'localhost'
 #username = 'username'
 #password = 'password'
 database = 'forthwind'
 
-def query( conn ) :
-    cur = conn.cursor()
-    cur.execute( "SELECT * FROM website.test" )
-    for i,j in cur.fetchall():
-        print(i,j)
-
 def getData(name) :
     conn = psycopg2.connect( host=hostname, user=username, password=password, database=database )
-    query( conn )
+    cur = conn.cursor()
+    cur.execute( "SELECT * FROM website.test" )
+    data = cur.fetchall()
     conn.close()
-
-    return "<html><p>This Works :)</p></html>"
+    
+    return data
