ebb1803d55e376f7c09082ee2ca4ecae0a3c1e85
klforthwind <kl.forthwind@gmail.com>
2019-05-21 07:46:56 +0000

    make variables shorter

--- PSQL.py
+++ PSQL.py
@@ -1,19 +1,21 @@
+from login import *
 import psycopg2
 
+
 hostname = 'localhost'
-username = 'username'
-password = 'password'
+#username = 'username'
+#password = 'password'
 database = 'forthwind'
 
-def doQuery( conn ) :
+def query( conn ) :
     cur = conn.cursor()
     cur.execute( "SELECT * FROM website.test" )
     for i,j in cur.fetchall():
         print(i,j)
 
 def getData(name) :
-    myConnection = psycopg2.connect( host=hostname, user=username, password=password, database=database )
-    doQuery( myConnection )
-    myConnection.close()
+    conn = psycopg2.connect( host=hostname, user=username, password=password, database=database )
+    query( conn )
+    conn.close()
 
     return "<html><p>This Works :)</p></html>"
--- login.py
+++ login.py
@@ -1,2 +1,2 @@
-username="fakeusername :)"
+username="fakeusername"
 password="fakepassword :)"
