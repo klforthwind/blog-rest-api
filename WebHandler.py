c385cf54d8c36aa597d20d90cd619291ce288924
klforthwind <kl.forthwind@gmail.com>
2019-05-22 14:56:14 +0000

    get rid of webhandler.py, as it wasn't needed

--- WebHandler.py
+++
@@ -1,6 +0,0 @@
-from PSQL import getData
-import markdown2
-
-def getWebsite(name) :
-    html = getData(name)
-    return html
--- rest.py
+++ rest.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from WebHandler import getWebsite
+from PSQL import getData
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,12 +9,12 @@ api = Api(app)
 class Blog(Resource):
 
     #GET Request- Returns website in full html
-    def get(self, name):
-        return getWebsite(name)
+    def get(self, url):
+        return getData(url)
         
-    #def post(self, name):
-    #def put(self, name):
-    #def delete(self, name):
+    #def post(self, url):
+    #def put(self, url):
+    #def delete(self, url):
 
 # Access the api from 198.58.107.98:6969/blog/url-name
 api.add_resource(Blog, "/blog/<string:name>")
