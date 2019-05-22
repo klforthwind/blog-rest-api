3c5cae72bb9298f90ab545da7d354ca42d7dbaca
klforthwind <kl.forthwind@gmail.com>
2019-05-22 15:09:41 +0000

    fix syntantical errors

--- rest.py
+++ rest.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from PSQL import getData
+from PSQL import issue_command
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,12 +9,12 @@ api = Api(app)
 class Blog(Resource):
 
     #GET Request- Returns website in full html
-    def get(self, url):
-        return getData(url)
+    def get(self, name):
+        return issue_command('SELECT * FROM website.blog WHERE url=\'{}\''.format(name), True);
         
-    #def post(self, url):
-    #def put(self, url):
-    #def delete(self, url):
+    #def post(self, name):
+    #def put(self, name):
+    #def delete(self, name):
 
 # Access the api from 198.58.107.98:6969/blog/url-name
 api.add_resource(Blog, "/blog/<string:name>")
