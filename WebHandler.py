69d890a3fc690020aab836a9ed62192ebd9aa071
klforthwind <kl.forthwind@gmail.com>
2019-05-22 15:54:25 +0000

    oops, semicolon habit

--- WebHandler.py
+++ WebHandler.py
@@ -3,7 +3,7 @@ from PSQL import issue_command
 def getHTML(url) :
 	top = open("top.txt","r")
 	bottom = open("bottom.txt", "r")
-	data = str(issue_command('SELECT html FROM website.blog WHERE url=\'{}\''.format(url), True)[0]);
+	data = str(issue_command('SELECT html FROM website.blog WHERE url=\'{}\''.format(url), True)[0])
 	content = data[2:-3]
 	print(content)
-	return str(top.read()) + content + str(bottom.read()); 
+	return str(top.read()) + content + str(bottom.read())
