7fe779025046a478a4c128ed80d17efced47ebe7
klforthwind <kl.forthwind@gmail.com>
2019-05-24 15:43:14 +0000

    add local array of posts

--- rest.py
+++ rest.py
@@ -6,26 +6,30 @@ import re
 app = Flask(__name__)
 api = Api(app)
 
+posts = {
+    "first-post"
+}
+
 # Blog REST API
 class Blog(Resource):
 
     #GET Request- Returns website in full html
     def get(self, name):
-        # Hoping this stops SQL injection
-        if re.match("^[A-Za-z0-9_-]*$", name):
-            #Get surrounding HTML
-            top = open("html/top.txt","r")
-            bottom = open("html/bottom.txt", "r")
+        for post in posts :
+            if (post==name) :
+                if re.match("^[A-Za-z0-9_-]*$", name):
+                    #Get surrounding HTML
+                    top = open("html/top.txt","r")
+                    bottom = open("html/bottom.txt", "r")
 
-            #Get Post HTML
-            fileLoc = "posts/" + str(name) + ".md"
-            data = open(fileLoc, "r")
-            content = markdown2.markdown(data.read())
+                    #Get Post HTML
+                    fileLoc = "posts/" + str(name) + ".md"
+                    data = open(fileLoc, "r")
+                    content = markdown2.markdown(data.read())
 
-            #Return the whole page in HTML
-            return str(top.read()) + content + str(bottom.read())
-        else:
-            return "RAWR XD"
+                    #Return the whole page in HTML
+                    return str(top.read()) + content + str(bottom.read())
+        return "RAWR XD"
         
     #def post(self, name):
     #def put(self, name):
