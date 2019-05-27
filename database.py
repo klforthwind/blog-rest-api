efb8acd792d3802ec1024fe248fbc95ef02705c9
klforthwind <kl.forthwind@gmail.com>
2019-05-27 21:46:30 +0000

    add post of blog creation

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
 posts = [
     {
+        "title": "How I Set Up This Blog",
+        "url": "blog-creation",
+        "date": "May 27th"
+    },
+    {
         "title": "Apache2 Concrete5 Website With phpMyAdmin",
         "url": "concrete5-website",
         "date": "May 26th"
